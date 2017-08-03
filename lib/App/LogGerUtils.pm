b098a6802c44146132433733e628309b46e47890
perlancar (on pc-office) <perlancar@gmail.com>
2017-08-03 12:35:25 +0700

    Initial commit

---
+++ lib/App/LogGerUtils.pm
@@ -0,0 +1,14 @@
+package App::LogGerUtils;
+
+# DATE
+# VERSION
+
+1;
+# ABSTRACT: Command-line utilities related to Log::ger
+
+=head1 SYNOPSIS
+
+This distribution provides the following command-line utilities related to
+L<Log::ger>:
+
+#INSERT_EXECS_LIST
